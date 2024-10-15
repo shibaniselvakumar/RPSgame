1f11b11ec9a5cf7e7d7f8623f0c3d6df131e8ee3
shibaniselvakumar <shibs0910@gmail.com>
2024-10-15 20:03:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var options=["rock","paper","scissors"];
+var randomnumber=Math.floor(Math.random()*3);
+var myOption=options[randomnumber];
+
+
+
+    $(".btn").click(function(){
+        var userOption=$(this).attr("id");
+        $("h1").css("color","white");
+
+        $(".btn").hide();
+        $("#"+myOption).show().append("<p>PC's Option</p>");
+        $("#"+userOption).show().append("<p>Your Option</p>");
+        if(myOption==userOption){
+            $("h1").text("Try again!😉")
+        }
+        else if((myOption=="paper"&&userOption=="rock")||(myOption=="rock"&&userOption=="scissors")||(myOption=="scissors"&&userOption=="paper")){
+            $("h1").text("You lose Kavi! HAHA😂");
+        }
+        else if((userOption=="paper"&&myOption=="rock")||(userOption=="rock"&&myOption=="scissors")||(userOption=="scissors"&&myOption=="paper")){
+            $("h1").text("You win!YAYY🥳");
+        }
+    });
